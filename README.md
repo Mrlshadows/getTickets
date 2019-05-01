c58677e2e1fed6c51f2a2ab7aa0d3cfc682bfe4c
Mrlshadows <3447668101@qq.com>
2019-05-02 00:01:12 +0800

    完整代码上传，但有bug

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ python获取火车票查询
 
 ## 总结
 verify=False 保证可以访问地址的时候不验证证书
+
+Python内置的@property装饰器就是负责把一个方法变成属性调用
--- code/get_tickets.py
+++ code/get_tickets.py
@@ -23,9 +23,12 @@ Example:
 from docopt import docopt
 import requests
 import urllib3
-import prettytable
-import colorama
+from prettytable import PrettyTable
+from colorama import init, Fore
 from stations import stations
+
+# 着色的初始化
+init()
 
 # 解析数据类
 class TrainsPrase:
@@ -39,10 +42,47 @@ class TrainsPrase:
         # options 存储查询的选项，高铁，动车等
         self.options = options
 
-    # 获取持续时间
+    # 获取持续时间，这个在哪里使用呢
     def _get_duration(self, raw_train):
         duration = raw_train.get('lishi').replace(':', '小时') + '分'
-        if duration.startswith('00')
+        if duration.startswith('00'):
+            return duration[4:]
+        if duration.startswith('0'):
+            return duration[1:]
+        return duration
+
+    # 火车的数据
+    @property
+    def trains(self):
+        for raw_train in self.available_trains:
+            train_no = raw_train['station_train_code']
+            initial = train_no[0].lower()
+            if not self.options or initial in self.options:
+                train = [
+                    train_no,
+                    '\n'.join([Fore.GREEN + raw_train['from_station_name' + Fore.RESET],
+                               Fore.RED + raw_train['to_station_name'] + Fore.RESET]),
+                    '\n'.join([Fore.GREEN + raw_train['start_time' + Fore.RESET],
+                               Fore.RED + raw_train['arrive_time'] + Fore.RESET]),
+                    self._get_duration(raw_train),
+                    raw_train['zy_num'],
+                    raw_train['ze_num'],
+                    raw_train['rw_num'],
+                    raw_train['yw_num'],
+                    raw_train['yz_num'],
+                    raw_train['wz_num'],
+                ]
+                yield train
+
+    # 以表格形式打印数据
+    def pretty_print(self):
+        pt = PrettyTable()
+        pt._set_field_names(self.header)
+        for train in self.trains:
+            pt.add_row(train)
+        print(pt)
+
+
 
 # 命令行接口
 def cli():
@@ -58,10 +98,17 @@ def cli():
         date, from_station, to_station
     )
 
-    # 访问获取result
+    # 可选项
+    options = ''.join([
+        key for key, value in arguments.items() if value is True
+    ])
+
+    # 访问获取result，并且格式化
     urllib3.disable_warnings()
     r = requests.get(url, verify=False)
-    print(r.json()['data']['result'])
+    print(r.json())
+    available_trains = r.json()['data']['result']
+    TrainsPrase(available_trains, options).pretty_print()
 
 # main 函数
 if __name__ == '__main__':
